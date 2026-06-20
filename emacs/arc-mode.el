30b317e7c4a092cd401219694d93602a9dc5cc86
VxidDev <stas050595@gmail.com>
2026-06-20 12:55:50 +0200

    feat: null highlighting

--- emacs/arc-mode.el
+++ emacs/arc-mode.el
@@ -16,7 +16,7 @@
     (,(regexp-opt
        '("IF" "THEN" "ELIF" "ELSE" "WHILE" "FOR" "IN"
          "END" "FN" "VAR" "RETURN" "IMPORT"
-         "TRY" "CATCH" "BREAK" "CONTINUE" "CLASS" "FALSE" "TRUE" "OR" "AND" "NOT")
+         "TRY" "CATCH" "BREAK" "CONTINUE" "CLASS" "FALSE" "TRUE" "OR" "AND" "NOT" "NULL")
        'words)
      . font-lock-keyword-face)
 
